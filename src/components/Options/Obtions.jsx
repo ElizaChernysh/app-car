e1a8b39afa7a8440a02c896d88127964b1268526
Eliza Chernysh <82468286+ElizaChernysh@users.noreply.github.com>
2022-08-10 14:13:41 +0300

    created wrapper

--- src/components/Options/Obtions.jsx
+++ src/components/Options/Obtions.jsx
@@ -1,37 +1,39 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import { useDispatch } from "react-redux";
-import {
-  getAllCars,
-} from "../../store/reducers/carsSlice";
+import { getAllCars } from "../../store/reducers/carsSlice";
 import { CustomSelect } from "../CustomSelect/CustomSelect";
 import { OptionComponent } from "./ObtionsSelect";
 import Cars from "../Cars/Cars.jsx";
 import { year, make, model, trim, mileage } from "./optionsList.jsx";
+import { useSelector } from "react-redux";
 import "./Options.scss";
 
 const Options = () => {
+  const cars = useSelector((state) => state.cars.cars);
   const dispatch = useDispatch();
-  
+
   return (
     <div className="Options">
       <div className="wrapper">
         <h2 className="Options__title">CURRENT OFFERS</h2>
         <div className="Options__selects">
-          <OptionComponent list={year} title={"Year"} changes={"byYear"} />
-          <OptionComponent list={make} title={"Make"} changes={"byMake"} />
-          <OptionComponent list={model} title={"Model"} changes={"byModel"} />
-          <OptionComponent list={trim} title={"Trim"} changes={"byTrim"} />
-          <OptionComponent
-            list={mileage}
-            title={"Mileage"}
-            changes={"byMileage"}
-          />
+          <div className="Options__selects-wrapper">
+            <OptionComponent list={year} title={"Year"} changes={"byYear"} />
+            <OptionComponent list={make} title={"Make"} changes={"byMake"} />
+            <OptionComponent list={model} title={"Model"} changes={"byModel"} />
+            <OptionComponent list={trim} title={"Trim"} changes={"byTrim"} />
+            <OptionComponent
+              list={mileage}
+              title={"Mileage"}
+              changes={"byMileage"}
+            />
+          </div>
           <button
             className="Options__button"
             onClick={() => dispatch(getAllCars())}
           >
-            All
+            {cars.length} cars
           </button>
         </div>
       </div>
